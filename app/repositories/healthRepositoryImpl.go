53e48d6ef3c24c5975add1196245336b32311be7
ChristYoga123 <christianuswibisono@gmail.com>
2025-06-15 13:00:33 +0700

    fix: fix repo

--- app/repositories/healthRepositoryImpl.go
+++ app/repositories/healthRepositoryImpl.go
@@ -2,17 +2,30 @@ package repositories
 
 import (
 	"go-skeleton/app/repositories/interfaces"
+	"go-skeleton/configs"
+
+	"gorm.io/gorm"
 )
 
-type healthRepository struct{}
+type healthRepository struct {
+	db *gorm.DB
+}
 
 // NewHealthRepository creates a new instance of health repository
 func NewHealthRepository() interfaces.HealthRepository {
-	return &healthRepository{}
+	return &healthRepository{
+		db: configs.GetDB(),
+	}
 }
 
 // Check implements the health check repository method
 func (r *healthRepository) Check() error {
-	// Add any database or external service checks here
-	return nil
+	// Check database connection
+	sqlDB, err := r.db.DB()
+	if err != nil {
+		return err
+	}
+
+	// Ping database
+	return sqlDB.Ping()
 } 
